12eb4e8e6f7c82b2a84c5455c71b6f0aaf94eccc
AdityaSingh27 <67536907+AdityaSingh27@users.noreply.github.com>
2020-07-19 19:36:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+function copy(){
+    alert('LOL!! Try not be lazy :)');
+
+}
+
+function summer() {
+    document.body.style.backgroundImage ="url('https://res.cloudinary.com/dkd5jyhic/image/upload/v1594968327/Summer_tf9yzu.png')";
+    document.body.style.backgroundRepeat="no-repeat";
+    document.body.style.backgroundSize="100%";
+    alert('Its Summer!');
+}
+
+function winter(){
+    document.body.style.backgroundImage ="url('https://res.cloudinary.com/pavie/image/upload/v1595005659/winter_kxcbyk.png')";
+    document.body.style.backroundRepeat="no-repeat";
+    document.body.style.backgroundSize="100%";
+    alert('Its Winter!');
+}
+
+function autumn(){
+    document.body.style.backgroundImage ="url('https://res.cloudinary.com/pavie/image/upload/v1595005628/autumn_ywwom5.png')";
+    document.body.style.backroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "100%";
+    alert('Its autumn!');
+}
+ function spring(){
+     document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005642/spring_qejh9v.png')";
+     document.body.style.backroundRepeat = "no-repeat";
+     document.body.style.backgroundSize = "100%";
+     alert('Its spring!');
+ }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
